67a61a35cd3e798c6031ac130b3b7c9acbe90be5
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-05-01 17:28:40 +0530

    some code

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
 
 // declare class
-
-
+import Shape from "./shape";
+export class Circle extends Shape {
+  constructor(name, pi, radius, color) {
+    super();
+  }
+  calculateArea() {
+    console.log(` ${this.pi} * ${this.radius} * ${this.radius}`);
+  }
+}
+let object1 = new Circle("newCircle", 3.14, 5, "brown");
+object1.calculateArea();
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
 // export class using module.exports
+export class Shape {
+  constructor(name, pi, radius, color = "green") {
+    this.name = name;
+    this.pi = pi;
+    this.radius = radius;
+    this.color = color;
+  }
+  drawShape() {
+    console.log(`${this.name}`);
+  }
+  calculateArea() {
+    console.log(` ${this.pi} * ${this.radius} * ${this.radius}`);
+  }
+}
+
+let object = new Shape("circle", 3.14, 4, "green");
+object.drawShape();
+object.calculateArea();
